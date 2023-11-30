b34b0b7cbfaf0a76a7f1ddda223c8100d8e99170
Gabe Nunez <gabe@gabenunez.com>
2023-11-30 17:02:40 -0600

    Console log error

--- pages/api/revalidate.tsx
+++ pages/api/revalidate.tsx
@@ -14,6 +14,7 @@ export default async function handler(req, res) {
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
+    console.error(err);
     return res.status(500).send("Error revalidating");
   }
 }
